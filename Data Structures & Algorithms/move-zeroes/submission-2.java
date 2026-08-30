994cc174237ff571208fe4626869c213c67f0d55
AndTech00 <65944439+AndTech00@users.noreply.github.com>
2026-08-30 14:50:45 +0200

    Add: move-zeroes - submission-2

---
+++ Data Structures & Algorithms/move-zeroes/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int l = 0; //Legge tutte l'array
+        int r = nums.length - 1;
+        int writer = 0; //Indica dove andare a scrivere i numeri !=0
+
+        while(l <= r){
+            if(nums[l] != 0){
+                int temp = nums[writer]; 
+                nums[writer] = nums[l];
+                nums[l] = temp;
+
+                writer++;
+            } 
+            l++;
+        }
+
+    }
+}
